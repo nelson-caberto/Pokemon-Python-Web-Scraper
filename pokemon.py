2bea0dd550b9d25c41356eb3ef4d51c2a6d5a701
Nelson Caberto <nelson-caberto@users.noreply.github.com>
2019-05-15 03:23:57 -0500

    Store GET requets locally
    Save data pulled from pokemon database to local files to prevent spamming the website.

--- pokemon.py
+++ pokemon.py
@@ -1,40 +1,78 @@
 from lxml import html, etree #etree needed for debug/printing htmlelement
 import requests
 import time
+import pickle
 
 import sys #using exit() for debugging
 
-#Pokemondb.net Pokedex Website
-url = "https://pokemondb.net/pokedex/all"
+import os #needed for file stuff
 
-#Create response object from URL
-page = requests.get(url)
+from pathlib import Path
 
-#ASSERT page is an html file
+debug = False
 
-#Create html object from raw page contents
-page_html = html.fromstring(page.content)
+print("Checking pokemon.http...", end='')
 
-#Create a list of paths to each pokemon
-pokemon_names = page_html.xpath('//a[@class="ent-name"]/@href')
+pokemon_http_file = Path('pokemon.http')
 
-#Prepend all entries with https://pokemondb.net
-pokemon_https = ["https://pokemondb.net{0}".format(name) for name in pokemon_names]
+if pokemon_http_file.is_file():
+	pokemon_https = pickle.load(open(pokemon_http_file, 'rb'))
+	print('Loaded')
+else:
+	#Pokemondb.net Pokedex Website
+	url = "https://pokemondb.net/pokedex/all"
+	
+	#Create response object from URL
+	page = requests.get(url)
+	
+	#ASSERT page is an html file
 
-#Remove Duplicates
-pokemon_https = list(dict.fromkeys(pokemon_https))
+	#Create html object from raw page contents
+	page_html = html.fromstring(page.content)
 
-#Remove Duplicates including original
-pokemon_https = [name for name in pokemon_names if pokemon_names.count(name) == 1]
+	#Create a list of paths to each pokemon
+	pokemon_names = page_html.xpath('//a[@class="ent-name"]/@href')
+
+	#Remove Duplicates
+#	pokemon_https = list(dict.fromkeys(pokemon_https))
+
+	#Remove Duplicates including original and remove "/pokedex/" from name
+	pokemon_https = [name[9:] for name in pokemon_names if pokemon_names.count(name) == 1]
+
+	#Write pokemon_https to pokemon_http_file
+	pickle.dump(pokemon_https, open(pokemon_http_file, 'wb'))
+	print('Downloaded and Saved')
+
 #print(pokemon_https)
 #sys.exit()
 
-#be nice and dont spam the site
-time.sleep(1)
+print("Checking pokemon folder...", end='')
+
+pokemon_dir = "pokemon"
+if not os.path.isdir(pokemon_dir):
+	os.mkdir(pokemon_dir)
+	print('Created')
+else:
+	print('Exists')
+
+
+#sys.exit()
 
 pokemon_no = 0
+pokemon = pokemon_https[pokemon_no]
+pokemon_file = Path(pokemon_dir + '/' + pokemon)
 
-page = requests.get(pokemon_https[pokemon_no])
+print(f'{pokemon}...', end='')
+
+if pokemon_file.is_file():
+	page = pickle.load(open(pokemon_file, 'rb'))
+	print("Loaded...", end='')
+else:
+	#be nice and dont spam the site
+	time.sleep(1)
+	page = requests.get("https://pokemondb.net/pokedex/" + pokemon)
+	pickle.dump(page, open(pokemon_file, 'wb'))
+	print("Downloaded...", end='')
 
 page_html = html.fromstring(page.content)
 
@@ -51,15 +89,16 @@ abilities   = page_html.xpath('(//table[@class="vitals-table"])[1]/tbody/tr[6]/t
 abilities_h = page_html.xpath('(//table[@class="vitals-table"])[1]/tbody/tr[6]/td/small/a/text()')
 local_no    = dict(zip(page_html.xpath('(//table[@class="vitals-table"])[1]/tbody/tr[7]/td/small/text()'), page_html.xpath('(//table[@class="vitals-table"])[1]/tbody/tr[7]/td/text()')))
 
-print(f'name: {name}')
-print(f'national no: {national_no}')
-print(f'type: {types}')
-print(f'species: {species}')
-print(f'height: {height}')
-print(f'weight: {weight}')
-print(f'abilities: {abilities}')
-print(f'abilities_h: {abilities_h}')
-print(f'local no: {local_no}')
+if debug:
+	print(f'name: {name}')
+	print(f'national no: {national_no}')
+	print(f'type: {types}')
+	print(f'species: {species}')
+	print(f'height: {height}')
+	print(f'weight: {weight}')
+	print(f'abilities: {abilities}')
+	print(f'abilities_h: {abilities_h}')
+	print(f'local no: {local_no}')
 
 #Training
 EV_yield        = page_html.xpath('(//table[@class="vitals-table"])[2]/tbody/tr[1]/td/text()')
@@ -70,11 +109,12 @@ base_friendship.extend(page_html.xpath('(//table[@class="vitals-table"])[2]/tbod
 base_exp        = page_html.xpath('(//table[@class="vitals-table"])[2]/tbody/tr[4]/td/text()')
 growth_rate     = page_html.xpath('(//table[@class="vitals-table"])[2]/tbody/tr[5]/td/text()')
 
-print(f'EV_yield: {EV_yield}')
-print(f'catch_rate: {catch_rate}')
-print(f'base_friendship: {base_friendship}')
-print(f'base_exp: {base_exp}')
-print(f'growth_rate: {growth_rate}')
+if debug:
+	print(f'EV_yield: {EV_yield}')
+	print(f'catch_rate: {catch_rate}')
+	print(f'base_friendship: {base_friendship}')
+	print(f'base_exp: {base_exp}')
+	print(f'growth_rate: {growth_rate}')
 
 #Breeding
 egg_groups = page_html.xpath('(//table[@class="vitals-table"])[3]/tbody/tr[1]/td/a/text()')
@@ -83,9 +123,10 @@ egg_cycles = page_html.xpath('(//table[@class="vitals-table"])[3]/tbody/tr[3]/td
 del egg_cycles[-1]
 egg_cycles.extend(page_html.xpath('(//table[@class="vitals-table"])[3]/tbody/tr[3]/td/small/text()'))
 
-print(f'egg_groups: {egg_groups}')
-print(f'gender: {gender}')
-print(f'egg_cycles: {egg_cycles}')
+if debug:
+	print(f'egg_groups: {egg_groups}')
+	print(f'gender: {gender}')
+	print(f'egg_cycles: {egg_cycles}')
 
 #Base stats
 hp             = page_html.xpath('(//table[@class="vitals-table"])[4]/tbody/tr[1]/td[1]/text()')
@@ -108,25 +149,26 @@ speed_min      = page_html.xpath('(//table[@class="vitals-table"])[4]/tbody/tr[6
 speed_max      = page_html.xpath('(//table[@class="vitals-table"])[4]/tbody/tr[6]/td[4]/text()')
 base_total     = page_html.xpath('(//table[@class="vitals-table"])[4]/tfoot/tr/td/b/text()')
 
-print(f'hp: {hp}')
-print(f'hp_min: {hp_min}')
-print(f'hp_max: {hp_max}')
-print(f'attack: {attack}')
-print(f'attack_min: {attack_min}')
-print(f'attack_max: {attack_max}')
-print(f'defense: {defense}')
-print(f'defense_min: {defense_min}')
-print(f'defense_max: {defense_max}')
-print(f'sp_attack: {sp_attack}')
-print(f'sp_attack_min: {sp_attack_min}')
-print(f'sp_attack_max: {sp_attack_max}')
-print(f'sp_defense: {sp_defense}')
-print(f'sp_defense_min: {sp_defense_min}')
-print(f'sp_defense_max: {sp_defense_max}')
-print(f'speed: {speed}')
-print(f'speed_min: {speed_min}')
-print(f'speed_max: {speed_max}')
-print(f'base_total: {base_total}')
+if debug:
+	print(f'hp: {hp}')
+	print(f'hp_min: {hp_min}')
+	print(f'hp_max: {hp_max}')
+	print(f'attack: {attack}')
+	print(f'attack_min: {attack_min}')
+	print(f'attack_max: {attack_max}')
+	print(f'defense: {defense}')
+	print(f'defense_min: {defense_min}')
+	print(f'defense_max: {defense_max}')
+	print(f'sp_attack: {sp_attack}')
+	print(f'sp_attack_min: {sp_attack_min}')
+	print(f'sp_attack_max: {sp_attack_max}')
+	print(f'sp_defense: {sp_defense}')
+	print(f'sp_defense_min: {sp_defense_min}')
+	print(f'sp_defense_max: {sp_defense_max}')
+	print(f'speed: {speed}')
+	print(f'speed_min: {speed_min}')
+	print(f'speed_max: {speed_max}')
+	print(f'base_total: {base_total}')
 
 #Type defenses
 nor      = page_html.xpath('(//table[@class="type-table type-table-pokedex"])[1]/tr[2]/td[1]/@title')
@@ -157,15 +199,16 @@ gro      = page_html.xpath('(//table[@class="type-table type-table-pokedex"])[1]
 gro.extend(page_html.xpath('(//table[@class="type-table type-table-pokedex"])[1]/tr[2]/td[9]/text()'))
 if len(gro) == 1: gro.extend('0')
 
-print(f'nor: {nor}')
-print(f'fir: {fir}')
-print(f'wat: {wat}')
-print(f'ele: {ele}')
-print(f'gra: {gra}')
-print(f'ice: {ice}')
-print(f'fig: {fig}')
-print(f'poi: {poi}')
-print(f'gro: {gro}')
+if debug:
+	print(f'nor: {nor}')
+	print(f'fir: {fir}')
+	print(f'wat: {wat}')
+	print(f'ele: {ele}')
+	print(f'gra: {gra}')
+	print(f'ice: {ice}')
+	print(f'fig: {fig}')
+	print(f'poi: {poi}')
+	print(f'gro: {gro}')
 
 fly      = page_html.xpath('(//table[@class="type-table type-table-pokedex"])[2]/tr[2]/td[1]/@title')
 fly.extend(page_html.xpath('(//table[@class="type-table type-table-pokedex"])[2]/tr[2]/td[1]/text()'))
@@ -195,17 +238,20 @@ fai      = page_html.xpath('(//table[@class="type-table type-table-pokedex"])[2]
 fai.extend(page_html.xpath('(//table[@class="type-table type-table-pokedex"])[2]/tr[2]/td[9]/text()'))
 if len(fai) == 1: fai.extend('0')
 
-print(f'fly: {fly}')
-print(f'psy: {psy}')
-print(f'bug: {bug}')
-print(f'roc: {roc}')
-print(f'gho: {gho}')
-print(f'dra: {dra}')
-print(f'dar: {dar}')
-print(f'ste: {ste}')
-print(f'fai: {fai}')
+if debug:
+	print(f'fly: {fly}')
+	print(f'psy: {psy}')
+	print(f'bug: {bug}')
+	print(f'roc: {roc}')
+	print(f'gho: {gho}')
+	print(f'dra: {dra}')
+	print(f'dar: {dar}')
+	print(f'ste: {ste}')
+	print(f'fai: {fai}')
 
 #Pokedex entries
 
 
 #print(f': {}')
+
+print('Scraped')
